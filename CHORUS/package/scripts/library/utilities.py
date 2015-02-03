172b905da3c8c54e78d5cbe3e8c047c263eb2164
ben <ben@ros.io>
2015-02-03 13:27:49 -0700

    Clean up utilities file

--- CHORUS/package/scripts/library/utilities.py
+++ CHORUS/package/scripts/library/utilities.py
@@ -1,9 +1,17 @@
+"""
+Utility methods for Chorus installation and management
+"""
+
 import os
 from subprocess import Popen, PIPE
 from textwrap import dedent
-from collections import namedtuple
 
 def cprint(color, message):
+    """
+    Prints a message to console in a given color.
+    Available colors: HEADER, OKBLUE, OKGREEN, WARNING
+    FAIL, BOLD, UNDERLINE
+    """
     colors = {
         'HEADER': '\033[95m',
         'OKBLUE': '\033[94m',
@@ -20,6 +28,11 @@ def cprint(color, message):
     print color + message + colors['ENDC']
 
 def create_user(user):
+    """
+    Creates a user on a POSIX compliant system,
+    returns the new user's uid and gid.  If the user
+    already exists, just returns their uid and gid.
+    """
     os.system(dedent("""
         USER=%s
         id $USER > /dev/null 2>1
@@ -34,31 +47,56 @@ def create_user(user):
         'gid': int(os.popen('id --group %s' % user).read().strip())
     }
 
-def demoter(userAccount):
+def _demoter(user):
+    """
+    Creates an executable method which changes the
+    current process's uid and gid to those given.
+    """
     def result():
-        os.setgid(userAccount['gid'])
-        os.setuid(userAccount['uid'])
+        """
+        Changes the uid and gid to their new values.
+        Have to change gid first.
+        """
+        os.setgid(user['gid'])
+        os.setuid(user['uid'])
     return result
 
-def run(cmd, options={}, communicate=None, user=None):
-    if (user != None):
-        options['preexec_fn'] = demoter(user)
+def run(cmd, options=None, communicate=None, user=None):
+    """
+    Run a given command with given options using Popen.
+    Optionally give a string to communicate to the process,
+    if this is done any errors will be caught and an exception raised.
+    Optionally give a user to run the process as, only works if the parent
+    process is run as root.
+    """
 
-    if (communicate != None):
+    # pylint: disable=W0142
+
+    if options == None:
+        options = {}
+
+    if user != None:
+        options['preexec_fn'] = _demoter(user)
+
+    if communicate != None:
         process = Popen(cmd,
-            stdin = PIPE, stdout = PIPE, stderr = PIPE,
-            bufsize = 1,
-            shell = True,
-            **options
-        )
+                        stdin=PIPE,
+                        stdout=PIPE,
+                        stderr=PIPE,
+                        bufsize=1,
+                        shell=True,
+                        **options
+                       )
         out, err = process.communicate(communicate)
         process.wait()
 
-        if (len(err) > 0):
-            raise Exception(err)
-        if (process.returncode != 0):
-            raise Exception("Non-zero exit code " + str(process.returncode))
+        if len(err) > 0:
+            raise Exception("Error running " + cmd + ": " + err)
+        if process.returncode != 0:
+            raise Exception(cmd + " gave non-zero exit code " + str(process.returncode) + ": " + out)
 
         return out
     else:
         return Popen(cmd, **options)
+
+    # pylint: enable=W0142
