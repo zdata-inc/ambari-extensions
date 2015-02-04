af009b0c0b5aa20dce82ce35b9f847ac7f8889d7
bdelamotte <bdelamotte2.0@gmail.com>
2015-02-04 16:32:02 -0700

    Add setupRepo.sh to vagrant scripts

---
+++ vagrant/setupRepo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PHD_DOWNLOAD_PATH=/vagrant/artifacts/PHD-2.1.0.0-175.tar.gz
+PADS_DOWNLOAD_PATH=/vagrant/artifacts/PADS-1.2.1.0-10335.tar.gz
+LOCAL_REPO_DIR=/var/www/html/phd/
+PHD_DIR=PHD-2.1.0.0-175
+
+if [ -f "$PHD_DOWNLOAD_PATH" -a -f "$PADS_DOWNLOAD_PATH" ]; then 
+  echo "Creating local repo"
+  yum install -y createrepo
+  mkdir -p $LOCAL_REPO_DIR
+  tar -xvf $PHD_DOWNLOAD_PATH -C $LOCAL_REPO_DIR
+  tar -xvf $PADS_DOWNLOAD_PATH -C $LOCAL_REPO_DIR
+  createrepo $LOCAL_REPO_DIR
+else
+  echo "Could not find neccesary files to create local repo"
+fi
