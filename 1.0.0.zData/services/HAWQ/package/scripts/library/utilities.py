246e01db54d13b3e7a00092170529fd176e3131f
ben <ben@ros.io>
2015-02-10 13:30:11 -0700

    By default don't duplicate lines when adding to bash profile

--- 1.0.0.zData/services/HAWQ/package/scripts/library/utilities.py
+++ 1.0.0.zData/services/HAWQ/package/scripts/library/utilities.py
@@ -1,8 +1,19 @@
 from resource_management import *
 import json
 
-def appendBashProfile(user, toBeAppended, run=False):
-    Execute('echo % >> "/home/%s/.bashrc"' % json.dumps(toBeAppended), user)
+def _fileContains(file, needle):
+    with open(file, 'r') as filehandle:
+        for line in filehandle.readlines():
+            if line.strip() == needle.strip():
+                return True;
+
+    return False;
+
+def appendBashProfile(user, toBeAppended, run=False, allowDuplicates=False):
+    bashrc = "/home/%s/.bashrc" % user
+    command = json.dumps(toBeAppended)[1:-1]
+    if not _fileContains(bashrc, toBeAppended) or allowDuplicates:
+        Execute("echo %s >> \"%s\"" % (json.dumps(toBeAppended), bashrc))
 
     if run:
         Execute(toBeAppended)
