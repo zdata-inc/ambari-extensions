f1756e65c99cf1a8940cad60d3e358d4ce9809d8
ben <ben@ros.io>
2015-02-10 14:08:38 -0700

    Refactor kernelParamters and helper methods, finish setKernelParamter

--- 1.0.0.zData/services/HAWQ/package/scripts/library/utilities.py
+++ 1.0.0.zData/services/HAWQ/package/scripts/library/utilities.py
@@ -1,25 +1,54 @@
 from resource_management import *
+from resource_management.core.exceptions import Fail
 import json
+import logging
 
-def _fileContains(file, needle):
-    with open(file, 'r') as filehandle:
-        for line in filehandle.readlines():
-            if line.strip() == needle.strip():
-                return True;
+def _lines_contain(haystack, needle):
+    for line in haystack:
+        if line.strip() == needle.strip():
+            return True;
 
     return False;
+
+def _file_contains(file, needle):
+    with open(file, 'r') as filehandle:
+        return _lines_contain(filehandle.readlines(), needle)
 
 def appendBashProfile(user, toBeAppended, run=False, allowDuplicates=False):
     bashrc = "/home/%s/.bashrc" % user
     command = json.dumps(toBeAppended)[1:-1]
-    if not _fileContains(bashrc, toBeAppended) or allowDuplicates:
-        Execute("echo %s >> \"%s\"" % (json.dumps(toBeAppended), bashrc))
+
+    with open(bashrc, 'a+') as filehandle:
+        if not _lines_contain(filehandle.readlines(), command):
+            print "Appending " + command
+            filehandle.write(format("{toBeAppended}\n"))
 
     if run:
         Execute(toBeAppended)
 
-def kernelParameters(parameters):
+def setKernelParameters(parameters):
     for key, value in parameters.iteritems():
-        print key + ": " + value
-        # Execute('sysctl -w %s=%s' % (key, value))
+        setKernelParameter(key, value)
     pass
+
+def setKernelParameter(name, value, logoutput=True):
+    logLine = "%s = %s" % (name, value)
+
+    try:
+        Execute('sysctl -w %s=%s' % (name, value), logoutput=False)
+        logLine += " Added"
+
+        with open('/etc/sysctl.conf', 'a+') as filehandle:
+            line = format("{name} = {value}\n")
+            if not _lines_contain(filehandle.readlines(), line):
+                logLine += " Saved"
+                filehandle.write(line)
+            else:
+                logLine += " Already Saved"
+
+    except Fail as exception:
+        logLine += " Bad"
+        pass
+    finally:
+        if logoutput:
+            print logLine
