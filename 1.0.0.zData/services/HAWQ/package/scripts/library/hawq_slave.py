c405689fa5122a084d370e386a74b3f1d798e349
ben <ben@ros.io>
2015-02-09 19:20:21 -0700

    Add skeleton of HAWQ rewrite

---
+++ 1.0.0.zData/services/HAWQ/package/scripts/library/hawq_slave.py
@@ -0,0 +1,15 @@
+import os
+from library import utilities
+from resource_management.core.exceptions import ComponentIsNotRunning
+from resource_management import *
+
+class HawqSlave(object):
+    def install(self):
+
+    def configure(self):
+
+    def start(self):
+
+    def stop(self):
+
+    def is_running(self):
