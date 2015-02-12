457c48df7005f3e3be17b89b121e6ec197afac72
ben <ben@ros.io>
2015-02-12 13:33:01 -0700

    Install screen/yum

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,6 +2,8 @@ STABLE_REPO_URL=http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/
 TRUNK_REPO_URL=http://s3.amazonaws.com/dev.hortonworks.com/ambari/centos6/1.x/updates/1.7.0.trunk/ambari.repo
 AMBARI_REPO=$TRUNK_REPO_URL
 
+yum install -y screen vim
+
 # Create shared RSA keys
 sudo su - <<'EOF'
 if [ ! -f ~/.ssh ]; then
