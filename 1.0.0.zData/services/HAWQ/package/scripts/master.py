d8cb3d963ab8070a7c3756f506aec4decbdd8b2f
ben <ben@ros.io>
2015-02-13 21:37:30 -0700

    Remove now redundant code

--- 1.0.0.zData/services/HAWQ/package/scripts/master.py
+++ 1.0.0.zData/services/HAWQ/package/scripts/master.py
@@ -19,41 +19,6 @@ class Master(Script):
     hawq_master.install(env)
     sys.exit(1)
 
-    # Add user
-    hawq_useradd()
-
-    # Create hawq_hostfile_segments file (Should contain secondary master and all segments for Hawq service)
-    f = open(params.hawq_hostfile_segments,'w')
-    for host in params.hawq_segment_hosts:
-      f.write(host.strip()+'\n')
-    f.close()
-
-    # Source hawq environment and perform the ssh key exchange (allows you to log in to all hosts as root user without a password prompt)
-    # Note: This may not be needed at all if Ambari install used 'root' account to install all agents
-    os.system(params.source_cmd + params.exkeys_cmd)
-   
-    # Source the path file from the HAWQ installation directory and do the ssh key exchange as the hawq user
-    os.system("su - "+ params.hawq_user +" -c \""+ params.source_cmd + params.exkeys_cmd +"\"")
-
-    # Modify system level settings for Hawq
-    hawq_modify_kernel_parameters()
-    hawq_modify_security_parameters()
-    hawq_modify_mount_options()
-
-    # Setup master data storage area
-    # FIXME Create the master data directory location on your standby master as well
-    os.system("mkdir -p " + params.MASTER_DIRECTORY)
-    os.system("chown -R "+ params.hawq_user +" "+ params.MASTER_DIRECTORY)
-    
-    # Create gpinitsystem_config file
-    hawq_create_gpinitsystem_config()
-    os.system("echo "+ params.export_mdd_cmd +" >> /home/"+params.hawq_user+"/.bashrc")
-    os.system(params.export_mdd_cmd)
-    os.system("su - "+ params.hawq_user +" -c \""+ params.export_mdd_cmd +"\"")
-    os.system("su - "+ params.hawq_user +" -c \""+ params.source_cmd + params.gpinitsystem_cmd +"\"")
-    
-    sys.exit(1)
-
   def stop(self, env):
     print 'Stop the Hawq Master';
   def start(self, env):
