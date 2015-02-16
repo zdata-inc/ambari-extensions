a21952fc82348a4b7f18d3220ebfd081aa53469c
ben <ben@ros.io>
2015-02-16 14:49:58 -0700

    Remove redundant lines

--- build/bootstrap-master.sh
+++ build/bootstrap-master.sh
@@ -8,14 +8,10 @@ sudo yum install -y ambari-server httpd
 sudo service postgresql initdb
 sudo ambari-server setup -s
 
-sudo sh -c 'echo "client.api.port=8080" >> /etc/ambari-server/conf/ambari.properties'
-
 sudo ambari-server start
 
 sudo chkconfig postgresql on
 sudo chkconfig ambari-server on
-
-sudo sh -c 'echo "export SERVICES=/var/lib/ambari-server/resources/stacks/HDP/2.0.6/services/" >> ~/.bashrc'
 
 echo 'URL: http://master.ambaricluster.local:8080'
 echo 'Default username and password: admin/admin'
