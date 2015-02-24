b2497f298d93afbbe1bf02ff26499e490ff6d434
ben <ben@ros.io>
2015-02-24 13:32:17 -0700

    Fix copy stack copying directory instead of files.
    Would copy src into 9.9.9.zData, instead of src's files into 9.9.9.zData

--- build/copy-stack.sh
+++ build/copy-stack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [ $EUID == 0 ] || exec sudo bash "$0" "$@"
 
-cp -R /vagrant/src/ /var/lib/ambari-agent/cache/stacks/HDP/9.9.9.zData
+cp -R /vagrant/src/* /var/lib/ambari-agent/cache/stacks/HDP/9.9.9.zData
