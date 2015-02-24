c85abdf9848c24d400cd66c6c8abf2090f874181
ben <ben@ros.io>
2015-02-24 12:03:28 -0700

    Fix parallel up always creating two slaves

--- build/up.sh
+++ build/up.sh
@@ -8,7 +8,7 @@ vagrant up master
 echo "Starting slaves"
 
 if type parallel &>/dev/null; then
-    seq -f 'slave%g' 2 | parallel --no-notice vagrant up
+    seq -f 'slave%g' $1 | parallel --no-notice vagrant up
 else
     for i in `seq -f 'slave%g' $1`; do
         vagrant up $i
