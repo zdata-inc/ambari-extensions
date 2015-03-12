affbeec1bd33f9d59da0da5cac817c2264202873
ben <ben@ros.io>
2015-03-12 13:34:43 -0600

    Add basics for hawq client code

--- src/services/HAWQ/package/scripts/client.py
+++ src/services/HAWQ/package/scripts/client.py
@@ -1,9 +1,23 @@
 import sys
 from resource_management import *
+from library import hawq_master, hawq
+
+
 class Client(Script):
   def install(self, env):
-    print 'Install the Hawq Client';
+    import params
+    hawq.add_psql_environment_variables()
+
+    # Create hadoop log directory for hawq user
+    # This allows hdfs commands to be run as hawq user without errors
+    Directory(
+        os.path.join('/var/log/hadoop', params.hawq_user),
+        action="create",
+        owner=params.hawq_user
+    )
+
   def configure(self, env):
-    print 'Configure the Hawq Client';
+    pass
+
 if __name__ == "__main__":
   Client().execute()
