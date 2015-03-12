ac8ffc885d286f6a734901ea53faf8dfff446de8
ben <ben@ros.io>
2015-03-12 13:43:05 -0600

    Create zData cache directory before copying to it if needed

--- build/copy-stack.sh
+++ build/copy-stack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 [ $EUID == 0 ] || exec sudo bash "$0" "$@"
 
+mkdir -p /var/lib/ambari-agent/cache/stacks/HDP/9.9.9.zData 2> /dev/null
 cp -R /vagrant/src/* /var/lib/ambari-agent/cache/stacks/HDP/9.9.9.zData
