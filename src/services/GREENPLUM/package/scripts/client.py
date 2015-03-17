528bd0aee6a9812dce9f5ac77a5111c996b6f6ae
ben <ben@ros.io>
2015-03-17 12:44:51 -0600

    Begin rewrite of GREENPLUM

--- src/services/GREENPLUM/package/scripts/client.py
+++
@@ -1,9 +0,0 @@
-import sys
-from resource_management import *
-class Client(Script):
-  def install(self, env):
-    print 'Install the Greenplum Client';
-  def configure(self, env):
-    print 'Configure the Greenplum Client';
-if __name__ == "__main__":
-  Client().execute()
