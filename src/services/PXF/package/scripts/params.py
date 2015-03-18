37efe56354faeb36ae9c8fd1f02d856c31c30ad9
bdelamotte <bdelamotte2.0@gmail.com>
2015-03-18 16:44:38 -0600

    Setup template and set params in environment

--- src/services/PXF/package/scripts/params.py
+++ src/services/PXF/package/scripts/params.py
@@ -1,8 +1,13 @@
 from resource_management import *
 import os
+import subprocess
 
 config = Script.get_config()
 tmp_dir = Script.get_tmp_dir()
+
+# FIXME Find a better way to learn java_home
+#java_home = subprocess.call("find / -name java -path *bin* 2> /dev/null | head -1 | sed 's/\/bin\/java//g'", shell=True)
+java_home = "/usr/jdk64/jdk1.7.0_67/jre"
 
 # Important directories
 pxf_root_path = "/usr/lib/gphd/pxf"
