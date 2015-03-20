78ba6543cc42890667b520f7ceaef7bcd9245274
bdelamotte <bdelamotte2.0@gmail.com>
2015-03-20 15:43:46 -0600

    Remove unused method

--- src/services/PXF/package/scripts/library/pxf.py
+++ src/services/PXF/package/scripts/library/pxf.py
@@ -1,8 +1,1 @@
-def is_running(pidFile):
-  try:
-    with open(pidFile, 'r') as filehandle:
-      pid = int(filehandle.readlines()[0])
-      return utilities.is_process_running(pidFile, pid)
-
-  except IOError:
-    return False
+# Shared methods for PXF can go here... 
