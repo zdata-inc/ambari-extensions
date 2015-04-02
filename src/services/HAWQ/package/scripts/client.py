e15482487951c825babbff5c15002aedb15ba97f
ben <ben@ros.io>
2015-04-02 13:36:04 -0600

    Fix whitespace

--- src/services/HAWQ/package/scripts/client.py
+++ src/services/HAWQ/package/scripts/client.py
@@ -3,22 +3,22 @@ import sys
 from resource_management import *
 from library import hawq_master, hawq
 
-
 class Client(Script):
-  def install(self, env):
-    import params
-    hawq.add_psql_environment_variables()
+    def install(self, env):
+        import params
+        hawq.add_psql_environment_variables()
 
-    # Create hadoop log directory for hawq user
-    # This allows hdfs commands to be run as hawq user without errors
-    Directory(
-        os.path.join('/var/log/hadoop', params.hawq_user),
-        action="create",
-        owner=params.hawq_user
-    )
+        # Create hadoop log directory for hawq user
+        # This allows hdfs commands to be run as hawq user without errors
+        Directory(
+            os.path.join('/var/log/hadoop', params.hawq_user),
+            recursive=True,
+            action="create",
+            owner=params.hawq_user
+        )
 
-  def configure(self, env):
+def configure(self, env):
     pass
 
 if __name__ == "__main__":
-  Client().execute()
+    Client().execute()
