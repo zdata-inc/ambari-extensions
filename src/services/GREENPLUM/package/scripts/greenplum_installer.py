c97e7fea98d38bb48e2778f53ca373b3d6f1bc7c
ben <ben@ros.io>
2015-04-21 15:57:09 -0600

    Add method docs where necessary

--- src/services/GREENPLUM/package/scripts/greenplum_installer.py
+++ src/services/GREENPLUM/package/scripts/greenplum_installer.py
@@ -11,8 +11,10 @@ from resource_management import *
 class GreenplumDistributed(object):
     @staticmethod
     def make_tmpfile(tmp_dir=None):
+        """Create a temporary named file and return its path."""
+
         filehandle, tmp_path = tempfile.mkstemp(dir=tmp_dir)
-        os.close(filehandle) # Don't need access to the file
+        os.close(filehandle) # Don't need a filehandle
         return tmp_path
 
     @classmethod
@@ -24,6 +26,7 @@ class GreenplumDistributed(object):
 
         The installer_path can be either a local filepath or URL (one which can be downloaded by urllib).
         """
+
         tmp_path = cls.make_tmpfile(tmp_dir)
 
         # Attempt to locate locallay
@@ -64,6 +67,8 @@ class GreenplumDistributed(object):
             os.remove(self.__file_path)
 
     def get_installer(self):
+        """Extract the installation script from the distributed zip file and return it as a GreenplumInstaller."""
+
         installer_file = GreenplumInstaller.find_installer_name(map(lambda fileinfo: fileinfo.filename, self.__get_archive().infolist()))
 
         if len(installer_file) != 1:
@@ -120,13 +125,17 @@ class GreenplumInstaller(object):
             os.remove(self.__file_path)
 
     def install_to(self, destination):
+        """Install this version of Greenplum into the given destination directory."""
+
         archive = self.__get_archive()
         archive.extractall(destination)
 
     def get_name(self):
+        """Get the name of the installation script."""
         return self.__filename
 
     def get_version(self):
+        """Get the version of Greenplum the installation script will install."""
         return self.__version
 
     def close(self):
@@ -136,6 +145,7 @@ class GreenplumInstaller(object):
         self.__archive = None
 
     def __get_archive(self):
+        """Lazy load and return installation archive."""
         if self.__archive != None:
             return self.__archive
 
