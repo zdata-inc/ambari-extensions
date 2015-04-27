5b24ff406904c68609eda66e85956a5247d303bf
ben <ben@ros.io>
2015-04-27 16:24:12 -0600

    Cleanup spacing, comments.

--- src/services/PXF/package/scripts/params.py
+++ src/services/PXF/package/scripts/params.py
@@ -1,32 +1,23 @@
-from resource_management import *
-import os
-import subprocess
+from os import path
 import utilities
+from resource_management import *
 
 config = Script.get_config()
 tmp_dir = Script.get_tmp_dir()
 
 java_home = utilities.detect_java_home()
-if not java_home:
-  print "Cannot automatically find JAVA_HOME"
-  raise StandardError
+if java_home == None:
+    raise StandardError('Cannot automatically find JAVA_HOME')
 
 # Important directories
 pxf_root_path = "/usr/lib/gphd/pxf"
 pxf_instance_root_path = "/var/gphd/pxf"
 pxf_etc_config_path = "/etc/gphd/pxf/conf"
 
-# Environment variables for PXF 
-pxf_env_script = os.path.join(pxf_etc_config_path, "pxf-env.sh")
-# Kerberos security configuration file for PXF service
-pxf_site = os.path.join(pxf_etc_config_path, "pxf-site.xml")
-# PXF profiles definition file
-pxf_profiles = os.path.join(pxf_etc_config_path, "pxf-profiles.xml")
-# Classpaths required to run PXF
-pxf_private_classpath = os.path.join(pxf_etc_config_path, "pxf-private.classpath")
-# Classpaths for custom connectors 
-pxf_public_classpath = os.path.join(pxf_etc_config_path, "pxf-public.classpath")
-# PXF Logging configuration 
-pxf_log4j_properties = os.path.join(pxf_etc_config_path, "pxf-log4j.properties")
-# PID file Location
-pxf_pid_file = os.path.join(pxf_instance_root_path, "pxf-service/logs/tcserver.pid");
+pxf_env_script = path.join(pxf_etc_config_path, "pxf-env.sh") # Environment variables for PXF 
+pxf_site = path.join(pxf_etc_config_path, "pxf-site.xml") # Kerberos security configuration file for PXF service
+pxf_profiles = path.join(pxf_etc_config_path, "pxf-profiles.xml") # PXF profiles definition file
+pxf_private_classpath = path.join(pxf_etc_config_path, "pxf-private.classpath") # Classpaths required to run PXF
+pxf_public_classpath = path.join(pxf_etc_config_path, "pxf-public.classpath") # Classpaths for custom connectors 
+pxf_log4j_properties = path.join(pxf_etc_config_path, "pxf-log4j.properties") # PXF Logging configuration
+pxf_pid_file = path.join(pxf_instance_root_path, "pxf-service/logs/tcserver.pid");
