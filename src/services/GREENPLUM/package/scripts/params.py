cf4e3006dd93e3cf23c0f4ff14722fb2688c0fe1
ben <ben@ros.io>
2015-05-19 14:26:37 -0600

    Add admin_group param

--- src/services/GREENPLUM/package/scripts/params.py
+++ src/services/GREENPLUM/package/scripts/params.py
@@ -14,6 +14,7 @@ set_kernel_parameters = default('configurations/greenplum-env/set_kernel_paramet
 installation_path = config['configurations']['greenplum-env']['installation_path']
 absolute_installation_path = path.join(installation_path, 'greenplum-db')
 admin_user = config['configurations']['greenplum-env']['admin_user']
+admin_group = config['configurations']['greenplum-env']['admin_group']
 admin_password = config['configurations']['greenplum-env']['admin_password']
 hashed_admin_password = utilities.crypt_password(admin_password)
 
