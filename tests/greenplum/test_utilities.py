c06f6f7781d65654182adec5f5485237373f2099
ben <ben@ros.io>
2015-06-09 16:39:32 -0600

    Change path range operator from .. to -.
    Standardizes the expression, Ambari uses - as well.  Issue #22.

--- tests/greenplum/test_utilities.py
+++ tests/greenplum/test_utilities.py
@@ -4,10 +4,20 @@ import utilities
 
 class TestUtilities(unittest.TestCase):
     def test_parse_pattern_sequence_parsing(self):
-        self.assertEqual(utilities.parse_path_pattern_expression('[1..2]', 5), ['1', '2', '1', '2', '1'])
-        self.assertEqual(utilities.parse_path_pattern_expression('[1..2]', 6), ['1', '2', '1', '2', '1', '2'])
+        self.assertEqual(
+            utilities.parse_path_pattern_expression('[1-2]', 5),
+            ['1', '2', '1', '2', '1']
+        )
 
-        self.assertEqual(utilities.parse_path_pattern_expression('/data[1..2]/primary', 3), ['/data1/primary', '/data2/primary', '/data1/primary'])
+        self.assertEqual(
+            utilities.parse_path_pattern_expression('[1-2]', 6),
+            ['1', '2', '1', '2', '1', '2']
+        )
+
+        self.assertEqual(
+            utilities.parse_path_pattern_expression('/data[1-2]/primary', 3),
+            ['/data1/primary', '/data2/primary', '/data1/primary']
+        )
 
 if __name__ == '__main__':
     unittest.main()
