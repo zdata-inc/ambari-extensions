fb9c274c118d5bafe32fb8d0db4ef9ce27b8a539
ben <ben@ros.io>
2015-06-11 15:03:23 -0600

    :penguin: Include set file permissions

--- build/bootstrap-master.sh
+++ build/bootstrap-master.sh
@@ -6,7 +6,7 @@ source /vagrant/build/functions.sh
 
 # Choose which function to run to setup Ambari version.
 ## setupPivotalAmbari only works on zdata extensions 0.4 release branch
-setupPivotalAmbari /vagrant/artifacts/AMBARI-1.7.1-87-centos6.tar
-#setupVanillaAmbari /vagrant/artifacts/ambari.repo
-setupPivotalSoftwareSpecificRepo /vagrant/artifacts/PHD-UTILS-1.1.0.20-centos6.tar
-setupPivotalSoftwareSpecificRepo /vagrant/artifacts/PHD-3.0.0.0-249-centos6.tar
+# setupPivotalAmbari /vagrant/artifacts/AMBARI-1.7.1-87-centos6.tar
+# setupPivotalSoftwareSpecificRepo /vagrant/artifacts/PHD-UTILS-1.1.0.20-centos6.tar
+# setupPivotalSoftwareSpecificRepo /vagrant/artifacts/PHD-3.0.0.0-249-centos6.tar
+setupVanillaAmbari /vagrant/artifacts/ambari.repo
